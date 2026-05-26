087c364079b1659919b9dcc39645c1cbeb41b801
Nate Shoffner <nate.shoffner@gmail.com>
2026-05-26 03:33:32 -0400

    simplify CF Access middleware to validate JWT only

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,17 +17,16 @@ export async function middleware(req: NextRequest) {
   }
 
   const token = req.headers.get('CF-Access-JWT-Assertion')
-  const loginUrl = `https://${process.env.CF_ACCESS_TEAM_DOMAIN}/cdn-cgi/access/login/${req.nextUrl.hostname}${req.nextUrl.pathname}?redirect_url=${encodeURIComponent(req.url)}`
 
   if (!token) {
-    return NextResponse.redirect(loginUrl)
+    return new NextResponse('Unauthorized', { status: 401 })
   }
 
   try {
     await jwtVerify(token, JWKS, { audience: aud })
     return NextResponse.next()
   } catch {
-    return NextResponse.redirect(loginUrl)
+    return new NextResponse('Unauthorized', { status: 401 })
   }
 }
 
