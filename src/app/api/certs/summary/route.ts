598f4b6468ed9115cb9fd2326242240c67e3d19d
Nate Shoffner <nate.shoffner@gmail.com>
2026-05-29 16:23:03 -0400

    Make cert route static

--- src/app/api/certs/summary/route.ts
+++ src/app/api/certs/summary/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server'
 import { getAllCertifications } from '@lib/certs'
 
+export const dynamic = 'force-static'
+
 const MONTHS: Record<string, number> = {
   Jan: 0, Feb: 1, Mar: 2, Apr: 3, May: 4, Jun: 5,
   Jul: 6, Aug: 7, Sep: 8, Oct: 9, Nov: 10, Dec: 11,
